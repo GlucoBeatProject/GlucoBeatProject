51581d4603d50e47f760ddcbe142fc7e8e12cec1
naya-h2 <hw7666@naver.com>
2025-07-20 03:57:36 +0900

    feat: 대시보드 UI 변경

--- frontend/src/app/(default)/page.tsx
+++ frontend/src/app/(default)/page.tsx
@@ -10,6 +10,7 @@ import {
 } from '@/components/ui/card';
 import { Switch } from '@/components/ui/switch';
 import { parseISO, format } from 'date-fns';
+import { CGM_MAX, CGM_MIN } from '@/const/cgm';
 
 export default async function Home() {
   const today = new Date();
@@ -43,97 +44,204 @@ export default async function Home() {
         ).json()
     );
 
+  const recentReport = await (
+    await fetch(`http://127.0.0.1:4000/reports?user_id=1`)
+  )
+    .json()
+    .then(async (data) =>
+      data.length > 0
+        ? await (
+            await fetch(`http://127.0.0.1:4000/reports/${data[0].report_id}`)
+          ).json()
+        : null
+    );
+
   return (
-    <div className="mt-4 flex flex-col gap-4">
-      <section className="grid grid-cols-1 md:grid-cols-6 gap-5">
-        <Card className="md:col-span-2 h-full">
-          <CardHeader>
-            <CardTitle>최근 혈당</CardTitle>
-            <CardDescription>오늘 기록된 가장 최근 혈당입니다.</CardDescription>
-          </CardHeader>
-          <CardContent className="mx-auto">
-            {initCgmData.length > 0 ? (
-              <div className="flex flex-col gap-2 items-center w-fit h-full justify-center">
-                <p className="text-4xl sm:text-5xl font-bold text-primary">
+    <main className="mt-4 lg:mt-6 flex flex-col gap-6">
+      <div className="flex flex-col gap-6 lg:grid lg:grid-cols-2 lg:gap-5">
+        <section className="flex flex-col gap-5">
+          <h3 className="font-bold">내 상태 확인하기</h3>
+          <Card>
+            <CardHeader>
+              <CardTitle>최근 혈당 수치</CardTitle>
+              <CardDescription>
+                오늘 기록된 가장 최근 혈당입니다.
+              </CardDescription>
+              {initCgmData[initCgmData.length - 1].cgm_day
+                .at(-1)
+                .cgm.toFixed(0) > CGM_MAX ||
+              initCgmData[initCgmData.length - 1].cgm_day
+                .at(-1)
+                .cgm.toFixed(0) < CGM_MIN ? (
+                <div className="py-3 px-4 rounded-md bg-primary/15 text-primary font-bold">
+                  ⚠️ 현재{' '}
                   {initCgmData[initCgmData.length - 1].cgm_day
                     .at(-1)
-                    .cgm.toFixed(2)}{' '}
-                  <span className="text-xs text-gray-400 md:text-sm">
-                    mg/dL
+                    .cgm.toFixed(0) > CGM_MAX
+                    ? '고'
+                    : '저'}
+                  혈당 상태입니다.
+                </div>
+              ) : null}
+            </CardHeader>
+            <CardContent className="mx-auto">
+              {initCgmData.length > 0 ? (
+                <div className="flex flex-col gap-2 items-center w-fit h-full justify-center">
+                  <p
+                    className={`text-4xl sm:text-5xl font-bold ${
+                      initCgmData[initCgmData.length - 1].cgm_day
+                        .at(-1)
+                        .cgm.toFixed(0) > CGM_MAX ||
+                      initCgmData[initCgmData.length - 1].cgm_day
+                        .at(-1)
+                        .cgm.toFixed(0) < CGM_MIN
+                        ? 'text-primary'
+                        : 'text-green-600'
+                    }`}
+                  >
+                    {initCgmData[initCgmData.length - 1].cgm_day
+                      .at(-1)
+                      .cgm.toFixed(2)}{' '}
+                    <span className="text-xs text-gray-400 md:text-sm">
+                      mg/dL
+                    </span>
+                  </p>
+                  <p className="text-xs text-gray-500 md:text-sm">
+                    {initCgmData[initCgmData.length - 1].cgm_day.at(-1).time}
+                  </p>
+                </div>
+              ) : (
+                <span className="text-xs text-gray-400 text-center py-6">
+                  데이터가 존재하지 않습니다.
+                </span>
+              )}
+            </CardContent>
+          </Card>
+          <div className="grid grid-cols-3 gap-5">
+            <Card className="col-span-2">
+              <CardHeader>
+                <CardTitle>최근 인슐린 주입</CardTitle>
+                <CardDescription>
+                  오늘 기록된 가장 최근 인슐린 주입량입니다.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="mx-auto">
+                {initCgmData.length > 0 ? (
+                  <div className="flex flex-col gap-2 items-center w-fit h-full justify-center">
+                    <p className="text-3xl font-bold">
+                      {initInsulinData[initInsulinData.length - 1].insulin_day
+                        .at(-1)
+                        .insulin.toFixed(6)}{' '}
+                      <span className="text-xs text-gray-400 md:text-sm">
+                        U
+                      </span>
+                    </p>
+                    <p className="text-3xl font-bold">
+                      {
+                        initInsulinData[
+                          initInsulinData.length - 1
+                        ].insulin_day.at(-1).algorithm
+                      }{' '}
+                      <span className="text-xs text-gray-400 md:text-sm">
+                        알고리즘
+                      </span>
+                    </p>
+                    <p className="text-xs text-gray-500 md:text-sm">
+                      {
+                        initInsulinData[initCgmData.length - 1].insulin_day.at(
+                          -1
+                        ).time
+                      }
+                    </p>
+                  </div>
+                ) : (
+                  <span className="text-xs text-gray-400 text-center py-6">
+                    데이터가 존재하지 않습니다.
                   </span>
-                </p>
-                <p className="text-xs text-gray-500 md:text-sm">
-                  {initCgmData[initCgmData.length - 1].cgm_day.at(-1).time},{' '}
-                  {
-                    initInsulinData[initInsulinData.length - 1].insulin_day[0]
-                      .algorithm
-                  }
-                </p>
-              </div>
-            ) : (
-              <span className="text-xs text-gray-400 text-center py-6">
-                데이터가 존재하지 않습니다.
-              </span>
-            )}
-          </CardContent>
-        </Card>
+                )}
+              </CardContent>
+            </Card>
+            <div className="flex flex-col gap-5 ">
+              <Card className="flex flex-col justify-between">
+                <CardHeader>
+                  <CardTitle>패치</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <Switch id="patch-sensor" />
+                </CardContent>
+              </Card>
 
-        <div className="md:col-span-1 flex flex-col gap-5 min-h-[200px]">
-          <Card className="flex-1 flex flex-col justify-between">
-            <CardHeader>
-              <CardTitle>패치</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <Switch id="patch-sensor" />
-            </CardContent>
-          </Card>
-
-          <Card className="flex-1 flex flex-col justify-between">
-            <CardHeader>
-              <CardTitle>CGM 센서</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <Switch id="cgm-sensor" defaultChecked />
-            </CardContent>
-          </Card>
-        </div>
-
-        {/* 의사의 진단 */}
-        <Card className="md:col-span-3 h-full p-6">
-          <CardHeader className="p-0 mb-3">
-            <div className="flex justify-between items-center">
-              <CardTitle className="text-lg font-semibold text-gray-800">
-                최근 의사의 진단
-              </CardTitle>
+              <Card className="flex flex-col justify-between">
+                <CardHeader>
+                  <CardTitle>CGM 센서</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <Switch id="cgm-sensor" defaultChecked />
+                </CardContent>
+              </Card>
             </div>
-            <CardDescription className="mt-2 text-sm text-gray-500">
-              진단일: {format(parseISO(recentDia.created_at), 'yyyy년 M월 d일')}
+          </div>
+        </section>
+
+        {/* 그래프 섹션 */}
+        <section className="flex flex-col gap-4">
+          <h3 className="font-bold">그래프 한 눈에 보기</h3>
+          <DashboardContent
+            initCgmData={initCgmData}
+            initInsulinData={initInsulinData}
+          />
+        </section>
+      </div>
+
+      <section className="flex flex-col gap-4">
+        <h3 className="font-bold">GlucoBeat와 건강 관리하기</h3>
+        <Card>
+          <CardHeader>
+            <CardTitle>최근 의사의 진단</CardTitle>
+            <CardDescription>
+              {format(parseISO(recentDia?.created_at), 'yyyy년 M월 d일 HH:mm')}
             </CardDescription>
           </CardHeader>
 
-          <CardContent className="text-sm leading-relaxed text-gray-800 whitespace-pre-line">
-            {recentDia.dia_message || '진단 내역이 없습니다.'}
+          <CardContent className="leading-relaxed text-gray-800 whitespace-pre-line">
+            {recentDia?.dia_message || '최근 진단 내역이 존재하지 않습니다.'}
           </CardContent>
 
-          <CardFooter className="justify-end p-0 mt-4">
+          <CardFooter>
             <Link
-              href={`/diagnosis/${recentDia.dia_id}`}
-              className="text-sm text-primary hover:underline"
+              href={`/diagnosis/${recentDia?.dia_id}`}
+              className="text-sm text-primary hover:underline flex justify-end w-full"
             >
-              더보기
+              본문 보러가기
+            </Link>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>최근 심층 분석 리포트</CardTitle>
+            <CardDescription>
+              {format(
+                parseISO(recentReport?.created_at),
+                'yyyy년 M월 d일 HH:mm'
+              )}
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent className="text-gray-800 text-[20px] font-bold">
+            {recentReport?.report_title ||
+              '최근 생성된 리포트가 존재하지 않습니다.'}
+          </CardContent>
+
+          <CardFooter>
+            <Link
+              href={`/report/${recentReport?.report_id}`}
+              className="text-sm text-primary hover:underline flex justify-end w-full"
+            >
+              리포트 보러가기
             </Link>
           </CardFooter>
         </Card>
       </section>
-
-      {/* 그래프 섹션 */}
-      <section className="flex flex-col gap-4">
-        <h3 className="font-bold">그래프 한 눈에 보기</h3>
-        <DashboardContent
-          initCgmData={initCgmData}
-          initInsulinData={initInsulinData}
-        />
-      </section>
-    </div>
+    </main>
   );
 }
--- frontend/src/components/dashboard/Chart.tsx
+++ frontend/src/components/dashboard/Chart.tsx
@@ -14,6 +14,7 @@ import {
   CardHeader,
   CardTitle,
 } from '../ui/card';
+import { CGM_MAX, CGM_MIN } from '@/const/cgm';
 
 const chartConfig = {
   cgm: {
@@ -49,6 +50,60 @@ export function Chart({ type, isRange, data }: Props) {
       theme: 'url(#colorPv)',
       dataKey: isRange ? 'insulin_mean' : 'insulin',
     },
+  };
+
+  const CustomTooltip = ({ active, payload, label }: any) => {
+    if (active && payload && payload.length) {
+      const data = payload[0].payload;
+
+      const formattedLabel = payload[0].payload.date
+        ? new Date(label).toLocaleDateString('en-US', {
+            month: 'short',
+            day: 'numeric',
+          })
+        : label;
+
+      return (
+        <div className="p-2 text-xs bg-white border rounded-md shadow-lg">
+          <p className="font-bold mb-2">{formattedLabel}</p>
+          {payload.map((p: any, index: any) => (
+            <div
+              key={index}
+              className="flex justify-between gap-4 items-center"
+            >
+              <p className="text-gray-400">{p.name}</p>
+              {p.name === 'cgm' ? (
+                <p
+                  className={`flex items-center ${
+                    p.value.toFixed(0) > CGM_MAX || p.value.toFixed(0) < CGM_MIN
+                      ? 'text-primary'
+                      : ''
+                  }`}
+                >
+                  {p.value.toFixed(4)}
+                  {p.value.toFixed(0) > CGM_MAX ||
+                  p.value.toFixed(0) < CGM_MIN ? (
+                    <span className="text-[10px] text-primary ml-1 border rounded-full border-primary px-[2.5px]">
+                      {p.value.toFixed(0) > CGM_MAX ? '고' : '저'}
+                    </span>
+                  ) : null}
+                </p>
+              ) : (
+                <p className={`flex items-center`}>{p.value.toFixed(4)}</p>
+              )}
+            </div>
+          ))}
+          {data.algorithm && (
+            <div className="flex justify-between gap-4 mt-1">
+              <p className="text-gray-400">사용 알고리즘</p>
+              <p>{data.algorithm}</p>
+            </div>
+          )}
+        </div>
+      );
+    }
+
+    return null;
   };
   return (
     <Card className="pt-0 gap-4">
@@ -91,21 +146,7 @@ export function Chart({ type, isRange, data }: Props) {
                 }}
               />
               <YAxis tickLine={false} axisLine={false} />
-              <ChartTooltip
-                cursor={false}
-                content={
-                  <ChartTooltipContent
-                    labelFormatter={(value) => {
-                      if (!isRange) return value;
-                      return new Date(value).toLocaleDateString('en-US', {
-                        month: 'short',
-                        day: 'numeric',
-                      });
-                    }}
-                    indicator="dot"
-                  />
-                }
-              />
+              <ChartTooltip cursor={false} content={<CustomTooltip />} />
               <Area
                 type="monotone"
                 dataKey={CHART_INFO[type].dataKey}
