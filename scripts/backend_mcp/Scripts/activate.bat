10dc7b14d2a18717a14fa71b0fe3be701151e51c
Seokgi Han <hski0930@gmail.com>
2025-07-19 20:36:15 +0900

    Setup Code Set

--- scripts/backend_mcp/Scripts/activate.bat
+++ scripts/backend_mcp/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\test\1\glucobeat-project\backend_mcp
+set VIRTUAL_ENV=E:\test\1\glucobeat-project\scripts\backend_mcp
 
 if not defined PROMPT set PROMPT=$P$G
 
--- scripts/simglucose_g2p2c/Scripts/activate.bat
+++ scripts/simglucose_g2p2c/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\test\1\glucobeat-project\simglucose_g2p2c
+set VIRTUAL_ENV=E:\test\1\glucobeat-project\scripts\simglucose_g2p2c
 
 if not defined PROMPT set PROMPT=$P$G
 
